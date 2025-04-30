0e29db62a93e60a84bc78be2deae6cd2673ed14d
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2025-04-30 21:48:46 +0530

    Update custom_dl.py

--- Lucia/util/custom_dl.py
+++ Lucia/util/custom_dl.py
@@ -3,12 +3,12 @@ import asyncio
 import logging
 from info import *
 from typing import Dict, Union
-from Lucia.Bot import work_loads
+from MoviePalace.Bot import work_loads
 from pyrogram import Client, utils, raw
 from .file_properties import get_file_ids
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
-from Lucia.server.exceptions import FIleNotFound
+from MoviePalace.server.exceptions import FIleNotFound
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
 
